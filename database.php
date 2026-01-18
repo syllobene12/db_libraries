6bea664f447dab9ce5532643efe50c53209f92f3
Syllobene12 <syllobenex1007@gmail.com>
2026-01-18 23:28:28 +0900

    Create database.php

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'host'     => 'localhost',
+    'dbname'   => 'test_db', // 実際のDB名に変更してください
+    'user'     => 'root',    // 実際のユーザー名に変更してください
+    'password' => 'root',    // 実際のパスワードに変更してください
+];
